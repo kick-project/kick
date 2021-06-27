d388a4caadb89d6a34a515ecd40cff72111a4645
Dexter Plameras <dexterp@gmail.com>
2021-06-28 06:33:16 +1000

    improve startcmd unit test

--- internal/subcmds/startcmd/startcmd_test.go
+++ internal/subcmds/startcmd/startcmd_test.go
@@ -2,11 +2,13 @@ package startcmd_test
 
 import (
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/kick-project/kick/internal/di"
 	"github.com/kick-project/kick/internal/resources/exit"
+	"github.com/kick-project/kick/internal/resources/marshal"
 	"github.com/kick-project/kick/internal/resources/testtools"
 	"github.com/kick-project/kick/internal/subcmds/startcmd"
 	"github.com/stretchr/testify/assert"
@@ -21,11 +23,27 @@ func TestStart(t *testing.T) {
 	tmpdir := testtools.TempDir()
 	home, _ := filepath.Abs(filepath.Join(tmpdir, "home"))
 	path, _ := ioutil.TempDir(tmpdir, "start-")
-	path = filepath.Join(path, "tmpl")
-	args := []string{"start", "tmpl", path}
+	project := "tmpl"
+	tmpl := "tmpl"
+	path = filepath.Join(path, project)
+	args := []string{"start", tmpl, path}
 	inject := di.New(&di.Options{
 		Home: home,
 	})
 	startcmd.Start(args, inject)
-	assert.DirExists(t, path)
+
+	type interpolated struct {
+		Project string `yaml:"project"`
+		Home    string `yaml:"home"`
+	}
+	d := &interpolated{}
+	y := filepath.Join(path, "template-interpolate.yml")
+	err := marshal.FromFile(d, y)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, d.Project, project)
+	assert.NotEmpty(t, os.Getenv("HOME"))
+	assert.Equal(t, d.Home, os.Getenv("HOME"))
 }
